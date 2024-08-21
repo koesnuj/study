d58e3b9dca31204a7df9942023b2e4ce5ba0f5e1
koesnuj <whizkid00@gmail.com>
2024-08-21 15:13:44 +0900

    say함수를 다른걸로 바꿈

--- class.py
+++ class.py
@@ -52,11 +52,11 @@ class QA:
         self.name = name
         self.lead = lead
         self.new0325 = new0325
-    def say(self):
+    def yell(self):
         print(f"QA 챕터의 인원은 {', '.join(self.name)}이고 챕터장은 {self.lead}지만, 나는 오버데어의 {self.new0325}다.")
 QAC = QA(["김성재", "이현호", "이현주", "최민철", "임준석"], "김성재", "이현호") #name에 해당하는 인원들은 []리스트로 묶고, lead와 new0325에 해당하는 사람은 ""문자열로 묶었다.
 
-QAC.say()
+QAC.yell()
 print()
  
 class Aespa:
@@ -64,25 +64,27 @@ class Aespa:
         self.name = name
         self.members = members
         self.songs = songs
-    def say(self):
+    def music(self):
         members_str = ", ".join(self.members)
         songs_str = ", ".join(self.songs)
         print(f'{self.name}의 멤버는 {members_str}이며 대표곡은 {songs_str}이다. 수수수 수퍼노바 노바 캔스탑 하이퍼 스텔라')
         
 aespa = Aespa('에스파', ['카리나', '윈터', '닝닝', '지젤'], ['NextLevel', 'BlackMamba', 'SuperNova'])
 
-aespa.say()
+aespa.music()
 print()
+
 class Redvelvet:
     def __init__(self, name, members, favorite):
         self.name = name
         self.members = members
         self.favorite = favorite
-    def say(self):
+    def like(self):
         members_str = ", ".join(self.members)
         print(f"{self.name}에는 {members_str}가 있는데 나의 최애는 {self.favorite}다.")
         
 redvelvet = Redvelvet('레드벨벳', ['아이린', '슬기', '예리', '조이', '웬디'], "슬기")
+print()
 
-redvelvet.say()
+redvelvet.like()
 print()
